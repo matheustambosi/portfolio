a8093288a3ccc18c96bd6b1eaafa78f34c29ada6
matheus.tambosi <matheus.tambosi@fulltechpro.com.br>
2023-11-10 17:25:13 -0300

    testando qrcode

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
 import { useSession } from "next-auth/react"
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+import { QrReader } from "react-qr-reader";
 
-export default function Home() {
+export default function Generate() {
+    const [data, setData] = useState("No result");
+    const [qrCodeValue, setQrCodeValue] = useState("");
+
     const { data: session, status } = useSession({
         required: true
     })
@@ -13,7 +19,34 @@ export default function Home() {
 
     return (
         <main className="min-h-screen flex justify-center items-center">
-            <h1>Dashboard</h1>
+            <div>Generate QR</div>
+
+            {qrCodeValue != "" && (
+                <QRCode value={qrCodeValue} />
+            )}
+
+            <QrReader
+                onResult={(result, error) => {
+                    if (!!result) {
+                        setData(result.getText);
+                    }
+
+                    if (!!error) {
+                        console.info(error);
+                    }
+
+                }
+                }
+                constraints={{ facingMode: "environment" }}
+                className="w-2/5 h-2/5"
+            />
+            <p>{data}</p>
+
+            <input
+                onChange={(e) => {
+                    setQrCodeValue(e.target.value);
+                }}
+            />
         </main>
-    )
+    );
 }
